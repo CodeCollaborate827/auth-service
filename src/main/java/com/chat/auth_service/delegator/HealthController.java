977d89108b0b0cc3e5ec4cd4833f24c5635f1d6a
Hai Nguyen <Hai.Nguyen@aeris.net>
2024-09-29 21:08:54 +0700

    Revert "registry with multipart/form"
    This reverts commit f2d8732fe9306389cb85ee14fdf4d3014b3bbd6d.

--- src/main/java/com/chat/auth_service/delegator/HealthController.java
+++ src/main/java/com/chat/auth_service/delegator/HealthController.java
@@ -1,14 +1,15 @@
 package com.chat.auth_service.delegator;
 
-import java.time.OffsetDateTime;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth/")
@@ -21,16 +22,17 @@ public class HealthController {
   @GetMapping("/health")
   public Map<String, Object> getHealth() {
     log.info("Health check via GET");
-    Map<String, Object> map = new HashMap<>();
+    Map<String, Object> map = new HashMap<String, Object>();
     map.put("name", applicationName);
     map.put("timestamp", OffsetDateTime.now());
     return map;
   }
 
+
   @PostMapping("/health")
   public Map<String, Object> postHealth() {
     log.info("Health check via POST");
-    Map<String, Object> map = new HashMap<>();
+    Map<String, Object> map = new HashMap<String, Object>();
     map.put("name", applicationName);
     map.put("timestamp", OffsetDateTime.now());
     return map;
